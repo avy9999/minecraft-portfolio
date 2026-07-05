738a80e6c5d924dd48baf38d766c5d8418f4c458
Aryan Gupta <totallymadgamer331@gmail.com>
2026-07-05 19:26:51 +0530

    Added Achievements section

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import "./Achievements.scss";
 
 const Achievements = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const achievements = [
+    {
+      title: "Achievement Made!",
+      content:
+        "2nd Runner Up at the 24-Hour Code Farming Hackathon held at KCC Institute of Technology and Management (KCC ITM).",
+      image: "/images/book.webp",
+    },
+    {
+      title: "Achievement Get!",
+      content:
+        "Serving as the Vice-President of the TechStars Club at KCC Institute of Technology and Management (KCC ITM).",
+      image: "/images/book.webp",
+    },
+  ];
 
-export default Achievements
+  return (
+    <div className="mc-achievements">
+      {achievements.map((achievement, index) => (
+        <div className="mc-achievement-card" key={index}>
+          <div className="mc-achievement-icon">
+            <img src={achievement.image} alt={achievement.title} />
+          </div>
+
+          <div className="mc-achievement-info">
+            <div className="mc-achievement-title">
+              {achievement.title}
+            </div>
+
+            <div className="mc-achievement-description">
+              {achievement.content}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Achievements;
